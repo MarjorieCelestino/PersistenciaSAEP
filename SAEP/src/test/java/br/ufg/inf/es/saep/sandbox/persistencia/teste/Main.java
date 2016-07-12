9339b2b50de5faa20bc92034c3c4c64029ce952b
MarjorieCelestino <marjorie_celestino@hotmail.com>
2016-07-12 19:33:11 -0300

    retiradas classes de teste

--- SAEP/src/test/java/br/ufg/inf/es/saep/sandbox/persistencia/teste/Main.java
+++ SAEP/src/test/java/br/ufg/inf/es/saep/sandbox/persistencia/teste/Main.java
@@ -4,6 +4,9 @@ package br.ufg.inf.es.saep.sandbox.persistencia.teste;
 import br.ufg.inf.es.saep.sandbox.dominio.Nota;
 import br.ufg.inf.es.saep.sandbox.dominio.Pontuacao;
 import br.ufg.inf.es.saep.sandbox.dominio.Valor;
+import br.ufg.inf.es.saep.sandbox.persistencia.FactoryParecer;
+import br.ufg.inf.es.saep.sandbox.persistencia.bo.ListaParecer;
+import br.ufg.inf.es.saep.sandbox.persistencia.transaction.parecer.ParecerCreateTransaction;
 import org.prevayler.Prevayler;
 import java.util.*;
 
